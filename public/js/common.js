a832f36c1021abae7fa3f2a9d96affb35c85e32f
Tharunraj Optimus <59702671+tharunoptimus@users.noreply.github.com>
2021-09-07 15:45:18 +0530

    Fixed bug where it won't accept the one drive urls

--- public/js/common.js
+++ public/js/common.js
@@ -1,1 +1,89 @@
-var qr,url="";async function userEnteredURL(){let e=$("#urlField").val().trim();isRealUrl(e)?await $.post("/api/links",{content:e},(e,n,o)=>{$("#shortenedUrlField").val(e.link),shorteningSuccessful(),url=e.link}):alert("Your URL is invalid!")}function shorteningSuccessful(){$("#homeContainer").hide(),$("#linkContainer").css("display","flex"),$("#linkContainer").hide(),$("#linkContainer").fadeIn("slow"),$(".topNavigation").css("display","flex"),$(".topNavigation").hide(),$(".topNavigation").fadeIn("slow"),copyFunction(),showCopySuccessfulBanner()}function isRealUrl(e){return/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(e)}function showCopySuccessfulBanner(){var e=$("#copySuccessfulBanner");e.addClass("show"),setTimeout(()=>{e.removeClass("show")},3e3)}function copyFunction(){var e=document.getElementById("shortenedUrlField");e.select(),e.setSelectionRange(0,99999),document.execCommand("copy")}function generateQR(e){qr=new QRious({element:document.getElementById("qr-code"),size:200,value:e}),$("#qrGeneratorButton").fadeOut("slow")}$("#urlField").keydown(e=>{13===e.keyCode&&userEnteredURL()}),$(document).on("click","#submitButton",()=>{userEnteredURL()}),$(document).on("click","#copyButton",()=>{copyFunction(),showCopySuccessfulBanner()}),$(document).on("click",".finalLinkContainer",e=>{$("#shortenedUrlField").select()}),$(document).on("click",".topNavigation",e=>{$("#urlField").val(""),$(".qrHolder").hide(),$("#linkContainer").hide(),$("#homeContainer").css("display","flex"),$("#homeContainer").hide(),$("#homeContainer").fadeIn("slow"),$(".topNavigation").fadeOut("slow")}),$(document).on("click",".generateQRButton",()=>{generateQR(url),$(".qrHolder").fadeIn("slow")}),"serviceWorker"in navigator&&window.addEventListener("load",()=>{navigator.serviceWorker.register("serviceworker.js").then(e=>console.log("Success: ",e.scope)).catch(e=>console.log("Failure: ",e))});
+var qr, url = ""
+async function userEnteredURL() {
+	let e = $("#urlField").val()
+	isRealUrl(e)
+		? await $.post("/api/links", { content: e }, (data, n, o) => {
+                console.log(data)
+				$("#shortenedUrlField").val(data.link),
+					shorteningSuccessful(),
+					(url = data.link)
+		  })
+		: alert("Your URL is invalid!")
+}
+
+function shorteningSuccessful() {
+	$("#homeContainer").hide()
+    $("#linkContainer").css("display", "flex")
+    $("#linkContainer").hide()
+    $("#linkContainer").fadeIn("slow")
+    $(".topNavigation").css("display", "flex")
+    $(".topNavigation").hide()
+    $(".topNavigation").fadeIn("slow")
+    copyFunction()
+    showCopySuccessfulBanner()
+}
+
+function isRealUrl(e) {
+	return  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/.test(e)
+}
+
+function showCopySuccessfulBanner() {
+	var e = $("#copySuccessfulBanner")
+	e.addClass("show")
+    setTimeout(() => {
+        e.removeClass("show")
+    }, 3e3)
+}
+
+function copyFunction() {
+	var e = document.getElementById("shortenedUrlField")
+	e.select(), e.setSelectionRange(0, 99999), document.execCommand("copy")
+}
+function generateQR(e) {
+	;(qr = new QRious({
+		element: document.getElementById("qr-code"),
+		size: 200,
+		value: e,
+	}))
+	$("#qrGeneratorButton").fadeOut("slow")
+}
+
+$("#urlField").keydown((e) => {
+	13 === e.keyCode && userEnteredURL()
+})
+
+$(document).on("click", "#submitButton", async () => {
+	await userEnteredURL()
+})
+
+$(document).on("click", "#copyButton", () => {
+	copyFunction()
+    showCopySuccessfulBanner()
+})
+
+$(document).on("click", ".finalLinkContainer", (e) => {
+	$("#shortenedUrlField").select()
+})
+
+$(document).on("click", ".topNavigation", (e) => {
+	$("#urlField").val("")
+	$(".qrHolder").hide()
+	$("#linkContainer").hide()
+	$("#homeContainer").css("display", "flex")
+	$("#homeContainer").hide()
+	$("#homeContainer").fadeIn("slow")
+	$(".topNavigation").fadeOut("slow")
+})
+
+$(document).on("click", ".generateQRButton", () => {
+	generateQR(url), $(".qrHolder").fadeIn("slow")
+})
+
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", () => {
+		navigator.serviceWorker
+			.register("serviceworker.js")
+			.then((reg) => console.log("Success: ", reg.scope))
+			.catch((err) => console.log("Failure: ", err))
+	})
+}
--- routes/api/links.js
+++ routes/api/links.js
@@ -1,62 +1,91 @@
-const express = require("express");
-const app = express();
-const router = express.Router();
-const Link = require("../../schemas/LinkSchema");
-app.use(express.urlencoded({extended: true}));
+const express = require("express")
+const crypto = require("crypto")
+const app = express()
+const router = express.Router()
+const Link = require("../../schemas/LinkSchema")
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 router.post("/", async (req, res, next) => {
-    if(!req.body.content) {
-        console.log("Content Parameter not sent with the request!")
-        return res.status(400);
-    }
-    
-    var fullUrl = req.protocol + '://' + req.get('host');
+	if (!req.body.content) {
+		console.log("Content Parameter not sent with the request!")
+		return res.status(400)
+	}
 
-    var postData = getShortUrl(req.body.content);
-    if(postData == null) { console.log("Invalid URL!"); return res.status(400)}
+	var fullUrl = req.protocol + "://" + req.get("host")
 
-    Link.create(postData)
-    .then(async (newPost) => {
-        var resource = {
-            url: newPost.url,
-            shortUrl: newPost.shortUrl,
-            link: fullUrl + "/" + newPost.shortUrl
-        }
-        res.status(201).send(resource)
-    })
-    .catch((error) => {
-        console.log(error);
-        res.sendStatus(400)
-    })
+	var postData = getShortUrl(req.body.content)
+	if (postData == null) {
+		console.log("Invalid URL!")
+		return res.status(400)
+	}
+
+	Link.create(postData)
+		.then(async (newPost) => {
+			var resource = {
+				url: newPost.url,
+				shortUrl: newPost.shortUrl,
+				link: fullUrl + "/" + newPost.shortUrl,
+			}
+			res.status(201).send(resource)
+		})
+		.catch((error) => {
+			console.log(error)
+			res.sendStatus(400)
+		})
 })
 
-function getShortUrl (url) {
-    if(isRealUrl(url)) {
-        var postData = {
-            url: url,
-            shortUrl: getRandom()
+function getShortUrl(url) {
+	if (isRealUrl(url)) {
+		var postData = {
+			url: url,
+			shortUrl: getRandom(),
+		}
+		return postData
+	} else {
+		return null
+	}
+}
+
+function isRealUrl(url) {
+	var regex =
+		/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+	return regex.test(url)
+}
+
+
+function generateForCustomCharacters(length, characters) {
+    const characterCount = characters.length
+    const maxValidSelector = Math.floor(0x10000 / characterCount) * characterCount - 1
+    const entropyLength = 2 * Math.ceil(1.1 * length)
+    let string = ""
+    let stringLength = 0
+
+    while (stringLength < length) {
+        const entropy = crypto.randomBytes(entropyLength)
+        let entropyPosition = 0
+
+        while (entropyPosition < entropyLength && stringLength < length) {
+            const entropyValue = entropy.readUInt16LE(entropyPosition)
+            entropyPosition += 2
+            if (entropyValue > maxValidSelector) {
+                continue
+            }
+
+            string += characters[entropyValue % characterCount]
+            stringLength++
         }
-        return postData;
     }
-    else {
-        return null;
-    }
+
+    return string
 }
 
-function isRealUrl (url) {
-    var regex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
-    return regex.test(url);
+function getRandom() {
+    let characters =
+	"AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzaAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0918273645546372819".split(
+		""
+	)
+    return generateForCustomCharacters(6, characters)
 }
 
-function getRandom () {
-    var words = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var randomWord = "";
-    for (var i = 0; i < 6; i++) {
-        var randomPosition = Math.floor(Math.random() * words.length);
-        randomWord += words.charAt(randomPosition);
-    }
-    return randomWord;
-}
-
-module.exports = router;
+module.exports = router
