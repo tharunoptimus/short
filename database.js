452777d3c541c87c0ae9e4fc5bfac0a72e7b1b16
tharunoptimus <tharunoptimus@outlook.com>
2022-01-30 02:23:22 +0530

    Changes according to new mongodb package

--- database.js
+++ database.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.set("useNewUrlParser", true );
-mongoose.set("useUnifiedTopology", true );
-mongoose.set("useFindAndModify", false );
-mongoose.set('useCreateIndex', true);
+
 class Database {
     constructor() {
         this.connect();
