833a9719f0c8e3691baee6c3516a499e0f7813f8
Tharunraj Optimus <tharunoptimus@outlook.com>
2022-09-11 08:40:29 +0000

    Replaced hardcoded db string with ENV

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3003;
--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-
+const MongoURI = process.env.MONGODB_URI
 class Database {
     constructor() {
         this.connect();
     }
     connect() {
-        mongoose.connect("mongodb+srv://username:username@projectname.i8met.mongodb.net/databaseName?retryWrites=true&w=majority")
+        mongoose.connect(MongoURI)
         .then(() => { console.log("Database Connection Successful!") })
         .catch((err) => { console.log("Unable to connect to the Database. Error: ", err) })
     }
